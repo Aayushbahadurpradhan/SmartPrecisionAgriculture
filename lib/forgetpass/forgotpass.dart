a721ea64e55ed31f194f21a255ecc3e0ba84df45
deeya2002 <diyadangol2002@gmail.com>
2023-06-12 14:26:35 +0545

    codes added

--- lib/forgetpass/forgotpass.dart
+++ lib/forgetpass/forgotpass.dart
@@ -1,1 +1,28 @@
-///file
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Login.dart';
+
+class ForgotScreen extends StatefulWidget {
+  const ForgotScreen({Key? key}) : super(key: key);
+
+  @override
+  _ForgotScreenState createState() => _ForgotScreenState();
+}
+
+class _ForgotScreenState extends State<ForgotScreen> {
+  final email = TextEditingController();
+
+
+
+  final form = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+      ),
+    );
+  }
+}
