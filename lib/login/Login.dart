657024b1a39c6cf183dc962e42f68a9131695466
pragya45 <pphuyal170@gmail.com>
2023-06-12 19:23:15 +0545

    login

---
+++ lib/login/Login.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import '../viewmodel/auth_view_model.dart';
+
+class LoginScreens extends StatefulWidget {
+  const LoginScreens({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreens> createState() => _LoginScreensState();
+}
+
+class _LoginScreensState extends State<LoginScreens> {
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: form,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 40),
+                  height: deviceHeight * 0.30,
+                  child: Image.asset('images/logo.png'),
+                ),
+                Container(
+                  height: deviceHeight * 0.75,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: LayoutBuilder(builder: (ctx, constraints) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(height: constraints.maxHeight * 0.04),
+                        Container(
+                          height: constraints.maxHeight * 0.12,
+                          decoration: BoxDecoration(
+                              color: const Color(0xffB4B4B4).withOpacity(0.6),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Center(
+                              child: TextFormField(
+                                controller: emailController,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Email is required";
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Email',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                       
+                        Container(
+                          width: double.infinity,
+                          height: constraints.maxHeight * 0.12,
+                          margin: EdgeInsets.only(
+                            top: constraints.maxHeight * 0.01,
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (form.currentState!.validate()) {
+                                login();
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                            ),
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
