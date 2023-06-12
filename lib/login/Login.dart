12ef25db1dfb5c9ec132e7c5daf3587cd9df2dc2
Biplope <maharjanbiplope16@gmail.com>
2023-06-12 20:59:46 +0545

    login

---
+++ lib/login/Login.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../register/signup.dart';
+
+class LoginScreens extends StatefulWidget {
+  const LoginScreens({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreens> createState() => _LoginScreensState();
+}
+
+class _LoginScreensState extends State<LoginScreens> {
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Form(
+            key: form,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 40),
+                  height: deviceHeight * 0.30,
+                  child: Image.asset('images/logo.png'),
+                ),
+
+
+
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
