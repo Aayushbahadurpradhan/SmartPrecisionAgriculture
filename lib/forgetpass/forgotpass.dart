2c704a41a9e9db394760e6928c7d32bd926ddb38
deeya2002 <deeyadangol2002@gmail.com>
2023-06-13 15:43:08 +0545

    forget pass added code

--- lib/forgetpass/forgotpass.dart
+++ lib/forgetpass/forgotpass.dart
@@ -1,6 +1,35 @@
-//file
+
+import 'package:flutter/material.dart';
 
 
+class ForgotScreen extends StatefulWidget {
+  const ForgotScreen({Key? key}) : super(key: key);
+
+  @override
+  _ForgotScreenState createState() => _ForgotScreenState();
+}
+
+class _ForgotScreenState extends State<ForgotScreen> {
+  final email = TextEditingController();
 
 
+  final form = GlobalKey<FormState>();
 
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Form(
+            key: form,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
