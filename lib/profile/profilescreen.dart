67f6a20d8f643dbc46e9a1936f6da65636c28874
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-06-14 12:59:22 +0545

    added some file

--- lib/profile/profilescreen.dart
+++ lib/profile/profilescreen.dart
@@ -1,1 +1,41 @@
-//created file
+import 'package:flutter/material.dart';
+
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+ TextEditingController _confirmPasswordController = TextEditingController();
+  bool _isEditing = false;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    if (user == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Padding(
+          ),
+        ),
+      ),
+    );
+  }
+}
