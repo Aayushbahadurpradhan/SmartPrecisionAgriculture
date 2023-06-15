cd0c0a2ea89079606d496bab1baa5c70cf82ec9b
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-06-15 13:07:32 +0545

    profile done

--- lib/profile/profilescreen.dart
+++ lib/profile/profilescreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'changepass.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -7,8 +10,88 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-
+  User? user;
+  late DocumentSnapshot userData;
   TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+  bool _isEditing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  void fetchUserData() async {
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      final userData = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser.uid)
+          .get();
+      setState(() {
+        user = currentUser;
+        this.userData = userData;
+        _usernameController.text = userData.get('username') ?? '';
+      });
+    }
+  }
+
+  void _logout(BuildContext context) async {
+    final confirmed = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Logout'),
+          content: Text('Are you sure you want to logout?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirmed == true) {
+      FirebaseAuth.instance.signOut();
+      Navigator.of(context).pushReplacementNamed('/login');
+    }
+  }
+
+  void _saveProfile() async {
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      if (_usernameController.text.isNotEmpty) {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(currentUser.uid)
+            .update({
+          'username': _usernameController.text,
+        });
+      }
+
+      setState(() {
+        _isEditing = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Profile updated successfully!'),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,6 +156,69 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                user?.email ?? 'N/A',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Contact',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'N/A', // Replace with actual contact value
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Date of Birth',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'N/A', // Replace with actual DOB value
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  if (_isEditing) {
+                    _saveProfile();
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => UpdateProfileScreen()),
+                    );
+                  }
+                },
+                child: Text(_isEditing ? 'Save Profile' : 'Update Profile'),
+                style: ElevatedButton.styleFrom(
+                  primary: _isEditing ? Colors.green : Colors.orange,
+                  textStyle: TextStyle(fontSize: 18),
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  _logout(context);
+                },
+                child: Text('Logout'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  textStyle: TextStyle(fontSize: 18),
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
                 ),
               ),
             ],
