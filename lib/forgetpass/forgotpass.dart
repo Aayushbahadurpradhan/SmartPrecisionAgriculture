7a93b8869c909b160f4ca754f7dd42baa218ebcf
deeya2002 <deeyadangol2002@gmail.com>
2023-06-15 15:21:47 +0545

    made change

--- lib/forgetpass/forgotpass.dart
+++ lib/forgetpass/forgotpass.dart
@@ -1,4 +1,5 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,6 +13,37 @@ class ForgotScreen extends StatefulWidget {
 class _ForgotScreenState extends State<ForgotScreen> {
   final email = TextEditingController();
 
+  @override
+  void dispose() {
+    email.dispose(); // TODO: implement dispose
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: email.text.trim());
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            //has to be valid email address for receiving the email
+            content: Text(' Check your email.'),
+          );
+        },
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(e.message.toString()),
+          );
+        },
+      );
+    }
+  }
 
   final form = GlobalKey<FormState>();
 
@@ -25,59 +57,30 @@ class _ForgotScreenState extends State<ForgotScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              ],
-            ),
+                Container(
+                  width: double.infinity,
+                  child: Image.asset(
+                    "images/logo.png",
+                    height: 300,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    child: Text(
+                      'Enter your email and we will send you a password reset link.',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
           ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
