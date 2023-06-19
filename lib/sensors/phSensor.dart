9f598c6697b0154c43d0d5a6c6b28e4eeded239d
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-06-19 11:21:56 +0545

    added some code

--- lib/sensors/phSensor.dart
+++ lib/sensors/phSensor.dart
@@ -1,1 +1,35 @@
-//created file
+import 'package:flutter/material.dart';
+class PHValueSensor extends StatefulWidget {
+  @override
+  _PHValueSensorState createState() => _PHValueSensorState();
+}
+
+class _PHValueSensorState extends State<PHValueSensor> {
+
+  double pH = 0.0;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('pH Value Sensor'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Current pH Value',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+    );
+  }
+}
+
+
