ba05bc45174a551f7aff0185bd8f1d0bf9f826a4
deeya2002 <diyadangol2002@gmail.com>
2023-06-19 16:46:28 +0545

    making changes

--- lib/profile/changepass.dart
+++ lib/profile/changepass.dart
@@ -1,1 +1,63 @@
-//file created
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UpdateProfileScreen extends StatefulWidget {
+  @override
+  _UpdateProfileScreenState createState() => _UpdateProfileScreenState();
+}
+
+class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
+  User? user;
+  late DocumentSnapshot userData;
+  TextEditingController _usernameController = TextEditingController();
+  bool _isEditing = false;
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    if (user == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Update Profile'),
+        ),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Update Profile'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.grey,
+              backgroundImage: NetworkImage(
+                'https://img.freepik.com/premium-vector/figure-person-hand-drawn-outline-doodle-icon-sketch-illustration-standing-figure-print-web-mobile-infographics-isolated-white-background_107173-17483.jpg', // Replace with image URL
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              controller: _usernameController,
+              enabled: _isEditing,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
