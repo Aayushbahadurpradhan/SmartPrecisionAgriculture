b0deb1f65460ed33846c72b52fa1cff3054d6118
Biplope <maharjanbiplope16@gmail.com>
2023-06-20 21:12:12 +0545

    motor

---
+++ lib/sensors/motor.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class MotorControlPage extends StatefulWidget {
+  @override
+  _MotorControlPageState createState() => _MotorControlPageState();
+}
+
+class _MotorControlPageState extends State<MotorControlPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Motor Control'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+
+
+
+        ),
+      ),
+    );
+  }
+}
