52aa583e2ea75734afb61ec552fd81d153a1f5ea
pragya45 <pphuyal170@gmail.com>
2023-06-21 19:40:52 +0545

    motor

--- lib/sensors/motor.dart
+++ lib/sensors/motor.dart
@@ -28,7 +28,36 @@ class _MotorControlPageState extends State<MotorControlPage> {
                 });
               },
             ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Change Motor 1 Name'),
+                    content: TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          motor1Name = text;
+                          setMotorName('motor1', motor1Name);
+                        });
+                      },
+                      controller: TextEditingController(text: motor1Name),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Close'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Text('Motor 1: $motor1Name'),
+            ),
             
+          ],
         ),
       ),
     );
