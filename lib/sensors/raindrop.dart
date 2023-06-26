e85ed6b681e3d68612d17928e31b368a52554626
deeya2002 <diyadangol2002@gmail.com>
2023-06-26 16:45:18 +0545

    added some code

--- lib/sensors/raindrop.dart
+++ lib/sensors/raindrop.dart
@@ -1,1 +1,42 @@
-//ccrea
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class RaindropSensorScreen extends StatefulWidget {
+  @override
+  _RaindropSensorScreenState createState() => _RaindropSensorScreenState();
+}
+
+class _RaindropSensorScreenState extends State<RaindropSensorScreen> {
+  DatabaseReference _databaseReference = FirebaseDatabase.instance.reference();
+
+  List<String> historyData = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Raindrop Sensor'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: historyData.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(historyData[index]),
+                );
+              },
+            ),
+          ),
+          TextButton(
+            child: Text('Add History Data'),
+            onPressed: () {
+              // Call the method to add new history data
+              _addHistoryData('New History Data');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
