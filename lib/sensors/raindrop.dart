b8b8be066a438faceff07e0b36cd1703db1dad83
deeya2002 <diyadangol2002@gmail.com>
2023-06-28 14:48:40 +0545

    code

--- lib/sensors/raindrop.dart
+++ lib/sensors/raindrop.dart
@@ -1,45 +1,116 @@
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class RaindropSensorScreen extends StatefulWidget {
+class RaindropSensor extends StatefulWidget {
   @override
-  _RaindropSensorScreenState createState() => _RaindropSensorScreenState();
+  _RaindropSensorState createState() => _RaindropSensorState();
 }
 
-class _RaindropSensorScreenState extends State<RaindropSensorScreen> {
-  DatabaseReference _databaseReference = FirebaseDatabase.instance.reference();
+class _RaindropSensorState extends State<RaindropSensor> {
+  DatabaseReference? _databaseRef;
+  FirebaseMessaging? _firebaseMessaging;
+  double raindropValue = 0;
+  List<Map<String, dynamic>> sensorData = [];
 
-  List<String> historyData = [];
+  FlutterLocalNotificationsPlugin? _flutterLocalNotificationsPlugin;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Raindrop Sensor'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: historyData.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(historyData[index]),
-                );
-              },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Raindrop Detection',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          TextButton(
-            child: Text('Add History Data'),
-            onPressed: () {
-              // Call the method to add new history data
-              _addHistoryData('New History Data');
-            },
-          ),
-        ],
+
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
