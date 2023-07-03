91345236b41e8bf33ef7020572a3bf287d5c9f99
pragya45 <pphuyal170@gmail.com>
2023-07-03 20:01:50 +0545

    ultrasonic test final

--- test/ultrasonic.dart
+++ test/ultrasonic.dart
@@ -6,6 +6,25 @@ import 'package:my_agri_project/main.dart';
 import 'package:my_agri_project/services/firebase_services.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    setupFirebaseCoreMocks();
+    await Firebase.initializeApp(
+      name: 'test',
+      options: const FirebaseOptions(
+        apiKey: 'test',
+        appId: 'test',
+        messagingSenderId: 'test',
+        projectId: 'test',
+      ),
+    );
+    final firestore = FakeFirebaseFirestore();
+    FirebaseService.db = firestore;
+
+    HttpOverrides.global = null;
+  });
+
+
   testWidgets('Ultrasonic UI Test', (WidgetTester tester) async {
     await tester.pumpWidget( MyApp(home: "/ultra",));
     await tester.pumpAndSettle();
