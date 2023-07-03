e196d3211f7f788918fd83a0c2de1d154f6271c1
pragya45 <pphuyal170@gmail.com>
2023-07-03 16:38:29 +0545

    raindropTest

--- test/raindropTest.dart
+++ test/raindropTest.dart
@@ -7,24 +7,21 @@ import 'package:my_agri_project/services/firebase_services.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    setupFirebaseCoreMocks();
+    await Firebase.initializeApp(
+      name: 'test',
+      options: const FirebaseOptions(
+        apiKey: 'test',
+        appId: 'test',
+        messagingSenderId: 'test',
+        projectId: 'test',
+      ),
+    );
 
-  // Build our app and trigger a frame.
-  await tester.pumpWidget( MyApp());
-
-  // Verify that our counter starts at 0.
-  expect(find.text('0'), findsOneWidget);
-  expect(find.text('1'), findsNothing);
-
-  // Tap the '+' icon and trigger a frame.
-  await tester.tap(find.byIcon(Icons.add));
-  await tester.pump();
-
-  // Verify that our counter has incremented.
-  expect(find.text('0'), findsNothing);
-  expect(find.text('1'), findsOneWidget);
-});
   testWidgets('Raindrop UI Test', (WidgetTester tester) async {
-    await tester.pumpWidget( MyApp(home: "/jjjj",));
+    await tester.pumpWidget( MyApp(home: "/rain",));
     await tester.pumpAndSettle();
 
     expect(find.text('Raindrop Sensor'), findsWidgets);
