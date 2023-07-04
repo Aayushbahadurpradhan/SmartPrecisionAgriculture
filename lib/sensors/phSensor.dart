59eee9e1113b5a9a0693c4b914c1d44d0b10a342
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2023-07-04 23:08:55 +0545

    finalising

--- lib/sensors/phSensor.dart
+++ lib/sensors/phSensor.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 class PHValueSensor extends StatefulWidget {
   @override
   _PHValueSensorState createState() => _PHValueSensorState();
@@ -12,13 +17,13 @@ class _PHValueSensorState extends State<PHValueSensor> {
 
   FlutterLocalNotificationsPlugin? _flutterLocalNotificationsPlugin;
 
-
   @override
   void initState() {
     super.initState();
-
-
+    initializeFirebase();
+    initializeNotifications();
   }
+
   Future<void> initializeFirebase() async {
     await Firebase.initializeApp();
     _databaseRef = FirebaseDatabase.instance.reference().child('test');
@@ -39,6 +44,54 @@ class _PHValueSensorState extends State<PHValueSensor> {
     });
   }
 
+  void initializeNotifications() {
+    _firebaseMessaging = FirebaseMessaging.instance;
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    final initializationSettingsAndroid =
+    AndroidInitializationSettings('launch_background');
+    final initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+    _flutterLocalNotificationsPlugin?.initialize(initializationSettings);
+
+    // Initialize Firebase Cloud Messaging
+  }
+
+  void checkMoistureLevel() {
+    if (pH < 7) {
+      sendNotification('pH is acidic!');
+    } else if (pH > 7) {
+      sendNotification('pH is base!');
+    } else if (pH == 7) {
+      sendNotification('pH is neutral');
+    }
+  }
+
+  Future<void> sendNotification(String message) async {
+    final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+  }
+
+  void showNotification(String? title, String? body) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title ?? ''),
+        content: Text(body ?? ''),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,51 +102,99 @@ class _PHValueSensorState extends State<PHValueSensor> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          Text(
-          'Current pH Value',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        SizedBox(height: 10),
-        Container(
-          width: 150,
-          height: 150,
-          decoration: BoxDecoration(
-            color: pH < 7
-                ? Colors.redAccent
-                : (pH > 7 ? Colors.blueAccent : Colors.greenAccent),
-            borderRadius: BorderRadius.circular(75),
-          ),
-          child: Center(
-            child: Text(
-              '${pH.toStringAsFixed(1)}',
+            Text(
+              'Current pH Value',
               style: TextStyle(
-                fontSize: 36,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
               ),
             ),
-          ),
-        ),
-        SizedBox(height: 20),
-        Text(
-          'History',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+            SizedBox(height: 10),
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: pH < 7
+                    ? Colors.redAccent
+                    : (pH > 7 ? Colors.blueAccent : Colors.greenAccent),
+                borderRadius: BorderRadius.circular(75),
+              ),
+              child: Center(
+                child: Text(
+                  '${pH.toStringAsFixed(1)}',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'History',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: Card(
+                elevation: 4,
+                child: sensorData.isEmpty
+                    ? Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                    columnSpacing: 20,
+                    headingRowHeight: 40,
+                    dataRowHeight: 56,
+                    horizontalMargin: 12,
+                    headingTextStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    dataTextStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                    columns: [
+                      DataColumn(label: Text('Timestamp')),
+                      DataColumn(label: Text('pH Value')),
+                    ],
+                    rows: sensorData
+                        .map(
+                          (data) => DataRow(
+                        cells: [
+                          DataCell(
+                            Text(data['timestamp'].toString()),
+                          ),
+                          DataCell(
+                            Text(
+                              '${data['pH'].toStringAsFixed(1)}',
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                        .toList(),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-    ),
     );
   }
-
-
-
-
 }
+
+
+
 
 
 
