8470122ab1730f54f3a09ad4f5668080af26dc59
HIVEND <shivendrashak12@gmail.com>
2023-08-11 23:11:11 +0545

    added

--- lib/register/signup.dart
+++ lib/register/signup.dart
@@ -31,21 +31,28 @@ class _RegisterScreenState extends State<RegisterScreen> {
     _ui = Provider.of<GlobalUIViewModel>(context, listen: false);
     super.initState();
   }
-  class _RegisterScreenState extends State<RegisterScreen> {
-  TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
-  TextEditingController confirmPassword = TextEditingController();
-  TextEditingController username = TextEditingController();
-  TextEditingController phoneNo = TextEditingController();
-  var _isVisible = false;
-  var _isVisibleConfirm = false;
-
-  late AuthViewModel _authViewModel;
-  late GlobalUIViewModel _ui;
-
-  @override
-  void initState() {
-  _authViewModel = Provider.of<AuthViewModel>(context, listen: false);
-  _ui = Provider.of<GlobalUIViewModel>(context, listen: false);
-  super.initState();
-  }
+  void register() async {
+    if (formkey.currentState == null || !formkey.currentState!.validate()) {
+      return;
+    }
+    _ui.loadState(true);
+    try {
+      await _authViewModel
+          .register(UserModel(
+        username: username.text,
+        email: email.text,
+        phone: phoneNo.text,
+        password: password.text,
+        id: '',
+      ))
+          .then((value) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Register success")));
+        // Navigator.of(context).pushReplacement(MaterialPageRoute(
+        //   builder: (BuildContext context) => OnboardingScreen(),
+        // ));
+        Navigator.of(context).pushReplacementNamed("/login");
+      }).catchError((e) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.message.toString())));
+      });
