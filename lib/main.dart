d6096f76555d17bbe9f02135ed1621a59e615117
deeya2002 <diyadangol2002@gmail.com>
2023-08-11 15:10:58 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -126,3 +123,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+
