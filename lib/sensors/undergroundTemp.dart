cb9448355e41d9e16be87460a003e9b197888250
HIVEND <shivendrashak12@gmail.com>
2023-08-11 23:20:12 +0545

    undergroud tem

---
+++ lib/sensors/undergroundTemp.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class WaterProofTemperatureSensor extends StatefulWidget {
+  @override
+  _WaterProofTemperatureSensorState createState() =>
+      _WaterProofTemperatureSensorState();
+}
