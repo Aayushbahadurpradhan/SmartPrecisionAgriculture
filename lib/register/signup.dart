9cc73fdd4f6701288b0e2d49cf8c7ac08c0c4dc5
HIVEND <shivendrashak12@gmail.com>
2023-08-11 23:07:50 +0545

    created regestration

---
+++ lib/register/signup.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/auth_model.dart';
+import '../viewmodel/auth_view_model.dart';
+import '../viewmodel/global_ui.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+
